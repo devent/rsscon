da21fb60815e9b3c4142a4bf90b3b021acb2a5f3
Erwin Mueller <erwin.mueller@deventm.org>
2010-09-13 21:47:07 +0200

    Put implementation in the c file
    To resolve a 'multiple definition of a function' errors.

---
+++ src/logger.c
@@ -0,0 +1,39 @@
+#include "logger.h"
+#include <stdarg.h>
+#include <log4c.h>
+
+log4c_category_t *get_log(const char* name) {
+	log4c_init();
+	return log4c_category_get(name);
+}
+
+int free_log() {
+	return log4c_fini();
+}
+
+void log_vdebug(const log4c_category_t *category, const char* format, va_list args){
+	log4c_category_vlog(category, LOG4C_PRIORITY_DEBUG, format, args);
+}
+
+void log_debug(const log4c_category_t *category, const char* format, ...) {
+	va_list va;
+	va_start(va, format);
+	log_vdebug(category, format, va);
+	va_end(va);
+}
+
+void log_enter(const log4c_category_t *category, const char* name, ...) {
+	va_list va;
+	va_start(va, name);
+	log_vdebug(category, name, va);
+	va_end(va);
+}
+
+void log_leave(const log4c_category_t *category, const char* name, ...) {
+	va_list va;
+	va_start(va, name);
+	log_debug(category, name, va);
+	va_end(va);
+}
+
+
--- src/logger.h
+++ src/logger.h
@@ -4,39 +4,17 @@
 #include <stdarg.h>
 #include <log4c.h>
 
-log4c_category_t *get_log(const char* name) {
-	log4c_init();
-	return log4c_category_get(name);
-}
+log4c_category_t *get_log(const char* name);
 
-int free_log() {
-	return log4c_fini();
-}
+int free_log();
 
-void log_vdebug(const log4c_category_t *category, const char* format, va_list args){
-	log4c_category_vlog(category, LOG4C_PRIORITY_DEBUG, format, args);
-}
+void log_vdebug(const log4c_category_t *category, const char* format, va_list args);
 
-void log_debug(const log4c_category_t *category, const char* format, ...) {
-	va_list va;
-	va_start(va, format);
-	log_vdebug(category, format, va);
-	va_end(va);
-}
+void log_debug(const log4c_category_t *category, const char* format, ...);
 
-void log_enter(const log4c_category_t *category, const char* name, ...) {
-	va_list va;
-	va_start(va, name);
-	log_vdebug(category, name, va);
-	va_end(va);
-}
+void log_enter(const log4c_category_t *category, const char* name, ...);
 
-void log_leave(const log4c_category_t *category, const char* name, ...) {
-	va_list va;
-	va_start(va, name);
-	log_debug(category, name, va);
-	va_end(va);
-}
+void log_leave(const log4c_category_t *category, const char* name, ...);
 
 
 #endif /* LOGGER_H_ */
