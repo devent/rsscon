4b9f37cd12c6df15d6ddc731f335a1545e5785c9
Erwin Mueller <erwin.mueller@deventm.org>
2012-11-08 00:14:14 +0100

    Add license headers

--- rsscon-shared/src/main/c/rssconwindows.c
+++ rsscon-shared/src/main/c/rssconwindows.c
@@ -1,21 +1,20 @@
 /**
- * Copyright 2010 Erwin Müller <erwin.mueller@deventm.org>
- * Version 1.0
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of rsscon.
+ * This file is part of rsscon-shared.
  *
- * rsscon is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
+ * rsscon-shared is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * rsscon is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * rsscon-shared is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Lesser General Public License along with
- * rsscon. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with rsscon-shared. If not, see <http://www.gnu.org/licenses/>.
  */
 #ifdef RSSCON_WINDOWS
 
