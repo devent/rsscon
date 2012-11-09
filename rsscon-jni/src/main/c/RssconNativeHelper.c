55265d8dc9e0394bf43ca5a7be4a2fb227cd4e4b
Erwin Mueller <erwin.mueller@deventm.org>
2012-11-09 01:55:23 +0100

    Add licenses headers

--- rsscon-jni/src/main/c/RssconNativeHelper.c
+++ rsscon-jni/src/main/c/RssconNativeHelper.c
@@ -1,20 +1,20 @@
 /**
- * Copyright 2011 Erwin Müller <erwin.mueller@deventm.org>
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of rssconjava-driver.
+ * This file is part of rsscon-jni.
  *
- * rssconjava-driver is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
+ * rsscon-jni is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * rssconjava-driver is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * rsscon-jni is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Lesser General Public License along with
- * rssconjava-driver. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with rsscon-jni. If not, see <http://www.gnu.org/licenses/>.
  */
 #include "com_anrisoftware_rsscon_nativeimpl_RssconNativeHelper.h"
 #include <rsscon.h>
