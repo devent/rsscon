5fc56abf0cd30e15a0dc462533c7a191643cd53b
Erwin Mueller <erwin.mueller@deventm.org>
2012-11-19 12:50:23 +0100

    Set license headers
    #1786

--- rsscon-jni/src/main/java/com/anrisoftware/rsscon/nativeimpl/RssconNativeImpl.java
+++ rsscon-jni/src/main/java/com/anrisoftware/rsscon/nativeimpl/RssconNativeImpl.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of nativeBinding.rsscon-jni.
+ * This file is part of rsscon-jni.
  *
- * nativeBinding.rsscon-jni is free software: you can redistribute it and/or modify it
+ * rsscon-jni is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * nativeBinding.rsscon-jni is distributed in the hope that it will be useful, but
+ * rsscon-jni is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with nativeBinding.rsscon-jni. If not, see <http://www.gnu.org/licenses/>.
+ * along with rsscon-jni. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.rsscon.nativeimpl;
 
